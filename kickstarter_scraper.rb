eb3dd4e84be316681404d3c1ac1dcc6e9136b6ac
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-25 05:09:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,12 @@
+# require libraries/modules here
+
+def create_project_hash
+
 html = File.read('fixtures/kickstarter.html')
  
 kickstarter = Nokogiri::HTML(html)
 
-def create_project_hash
-  # write your code here
+
+
+
 end
